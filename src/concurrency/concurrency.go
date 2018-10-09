c9a145c8d1fd44ca0d26529cd43556a1ebacb983
Parikshit Manur <pari.manur@gmail.com>
2018-10-09 20:26:02 +0530

    Adding channel support

--- src/concurrency/concurrency.go
+++ src/concurrency/concurrency.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
 func main() {
 
+	fmt.Println("Started runBoring")
 	runBoring()
+	fmt.Println("Finished runBoring\n\n")
 
+	time.Sleep(5 * time.Second)
+
+	fmt.Println("Starting runBoringWithChan")
+	runBoringWithChan()
+	fmt.Println("Finished runBoringWithChan\n\n")
+}
+
+func runBoringWithChan() {
+	c := make(chan string)
+	go boringWithChan("boring with chan c!", c)
+	for i := 0; i < 5; i++ {
+		fmt.Printf("You say: %q\n", <-c) // wait for a value to be sent.
+	}
+	fmt.Println("You're boring; I'm leaving.")
 }
 
 func runBoring() {
@@ -17,13 +34,18 @@ func runBoring() {
 	go boring("boring go routine!!")
 	// Running the below function will wait till the loop is completed
 	//boring("boring main!")
-	time.Sleep(2 * time.Second)
-	fmt.Println("You're boring; I'm leaving.")
+	time.Sleep(5 * time.Second)
+}
 
+func boringWithChan(msg string, c chan string) {
+	for i := 0; ; i++ {
+		c <- fmt.Sprintf("%s %d", msg, i) // It waits for a receiver to be ready.
+		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+	}
 }
 
 func boring(msg string) {
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 3; i++ {
 		fmt.Println(msg, i)
 		time.Sleep(time.Second)
 	}
