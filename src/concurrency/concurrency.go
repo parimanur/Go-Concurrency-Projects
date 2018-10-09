3e7ab092c6460f412bc58b4f03a2759439d38595
Parikshit Manur <pari.manur@gmail.com>
2018-10-09 22:09:40 +0530

    Fan in support and support to do cleanup using quit channel

--- src/concurrency/concurrency.go
+++ src/concurrency/concurrency.go
@@ -30,6 +30,16 @@ func main() {
 	runSyncCommunicationPattern()
 	fmt.Println("Finished Sync Communication Pattern\n\n")
 
+	time.Sleep(3 * time.Second)
+
+	fmt.Println("Starting Fan In Pattern")
+	runFanIn()
+	fmt.Println("Finished Fan In Pattern\n\n")
+
+	fmt.Println("Starting cleanup support using quit channel")
+	runmMssagePassingWithCleanup()
+	fmt.Println("Finished cleanup support using quit channel\n\n")
+
 }
 
 func runBoring() {
@@ -66,6 +76,81 @@ func runSyncCommunicationPattern() {
 		fmt.Println("Foo chan emits", <-fooChan)
 		fmt.Println("bar chan emits", <-barChan) //This is blocked till fooChan emits
 	}
+}
+
+func runFanIn() {
+	c := fanIn(boringChanGenerator("FooChan"), boringChanGenerator("BarChan"))
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-c)
+	}
+	fmt.Println("You're both boring; I'm leaving.")
+}
+
+func fanIn(input1, input2 <-chan string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for {
+			c <- <-input1
+		}
+	}()
+	go func() {
+		for {
+			c <- <-input2
+		}
+	}()
+	return c
+}
+
+func fanInWithSelect(input1, input2 <-chan string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for {
+			select {
+			case s := <-input1:
+				c <- s
+			case s := <-input2:
+				c <- s
+			}
+		}
+	}()
+	return c
+}
+
+func runmMssagePassingWithCleanup() {
+
+	quit := make(chan string) // This channel is used for communication to quit and respond saying i am done from caller
+	c := messagePassingWithCleanup("Joe", quit)
+
+	for i := rand.Intn(10); i >= 0; i-- {
+		fmt.Println(<-c)
+	}
+
+	quit <- "Bye!"                       // Quit signal main to caller
+	fmt.Printf("Joe Says: %q\n", <-quit) // Caller responds after he finishes the cleanup
+	//This is reached only after cleanup is done successfully
+}
+
+func messagePassingWithCleanup(msg string, quit chan string) <-chan string { // Returns receive-only (<-) channel of strings.
+	c := make(chan string)
+
+	go func() { // Launch the goroutine from inside the function. Function Literal.
+		for i := 0; ; i++ {
+			select {
+			case c <- fmt.Sprintf("%s %d", msg, i):
+				// Do Nothing.
+			case <-quit:
+				cleanup()
+				quit <- "See you!"
+				return
+			}
+		}
+	}()
+
+	return c // Return the channel to the caller.
+}
+
+func cleanup() {
+	fmt.Println("Cleaned Up")
 }
 
 func boring(msg string) {
