662a5f9a3314e8a1cddf223cad5bece45dca625c
Parikshit Manur <pari.manur@gmail.com>
2018-10-09 21:39:28 +0530

    Adding support for Generator Pattern and Channel Synchnorization

--- src/concurrency/concurrency.go
+++ src/concurrency/concurrency.go
@@ -12,20 +12,24 @@ func main() {
 	runBoring()
 	fmt.Println("Finished runBoring\n\n")
 
-	time.Sleep(5 * time.Second)
+	time.Sleep(3 * time.Second)
 
-	fmt.Println("Starting runBoringWithChan")
+	fmt.Println("Starting message passing using channel")
 	runBoringWithChan()
-	fmt.Println("Finished runBoringWithChan\n\n")
-}
+	fmt.Println("Finished message passing using channel\n\n")
 
-func runBoringWithChan() {
-	c := make(chan string)
-	go boringWithChan("boring with chan c!", c)
-	for i := 0; i < 5; i++ {
-		fmt.Printf("You say: %q\n", <-c) // wait for a value to be sent.
-	}
-	fmt.Println("You're boring; I'm leaving.")
+	time.Sleep(3 * time.Second)
+
+	fmt.Println("Starting Generator Pattern")
+	runBoringChanGenerator()
+	fmt.Println("Finished Generator Pattern\n\n")
+
+	time.Sleep(3 * time.Second)
+
+	fmt.Println("Starting Sync Communication Pattern")
+	runSyncCommunicationPattern()
+	fmt.Println("Finished Sync Communication Pattern\n\n")
+
 }
 
 func runBoring() {
@@ -34,7 +38,41 @@ func runBoring() {
 	go boring("boring go routine!!")
 	// Running the below function will wait till the loop is completed
 	//boring("boring main!")
-	time.Sleep(5 * time.Second)
+
+	time.Sleep(3 * time.Second)
+}
+func runBoringWithChan() {
+
+	c := make(chan string)
+	go boringWithChan("chan c passed to go routine!", c)
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Fetched from channel %q\n", <-c) // wait for a value to be sent.
+	}
+	fmt.Println("You're boring; I'm leaving.")
+}
+
+func runBoringChanGenerator() {
+	c := boringChanGenerator("Generator Pattern!")
+	for i := 0; i < 5; i++ {
+		fmt.Printf("%q\n", <-c)
+	}
+}
+
+func runSyncCommunicationPattern() {
+	fooChan := boringChanGenerator("foo")
+	barChan := boringChanGenerator("bar")
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("Foo chan emits", <-fooChan)
+		fmt.Println("bar chan emits", <-barChan) //This is blocked till fooChan emits
+	}
+}
+
+func boring(msg string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(msg, i)
+		time.Sleep(time.Second)
+	}
 }
 
 func boringWithChan(msg string, c chan string) {
@@ -44,9 +82,13 @@ func boringWithChan(msg string, c chan string) {
 	}
 }
 
-func boring(msg string) {
-	for i := 0; i < 3; i++ {
-		fmt.Println(msg, i)
-		time.Sleep(time.Second)
-	}
+func boringChanGenerator(msg string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return c
 }
